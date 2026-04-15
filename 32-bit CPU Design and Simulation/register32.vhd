398242f2efa16b82f2e89bbab3e51c3fa7d8c038
Sarojinder <sarojsanghera@gmail.com>
2026-04-14 22:20:02 -0400

    Initial commit

---
+++ 32-bit CPU Design and Simulation/register32.vhd
@@ -0,0 +1,26 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+ENTITY register32 IS
+PORT(
+		d		:IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+	  ld		:IN STD_LOGIC;
+	 clr		:IN STD_LOGIC;
+	 clk		:IN STD_LOGIC;
+	 Q       :OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+END register32;
+
+ARCHITECTURE description OF register32 IS
+BEGIN
+	process(ld, clr, clk)
+	begin
+		if clr ='1' then
+			Q <= (others =>'0');
+		elsif ((clk'event and clk = '1') and (ld = '1')) then
+			Q <= d;
+		end if;
+		
+	end process;
+END description;
